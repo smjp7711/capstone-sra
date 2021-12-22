c28e6c9b3ff41ae2a0df5d0aa3e7fff639cd99c7
nblarimore <nblarimore@ualr.edu>
2021-12-22 13:37:08 -0600

    Initial deployment

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost/sra'
 else:
+    app.debug = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://mhgqtisxrnhajo:47830ca44a024943e000deda5eb86a37b84fb1e4dd79bec1cc233702ce32a99e@ec2-3-225-132-26.compute-1.amazonaws.com:5432/d3m22ojr7fksk2'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
