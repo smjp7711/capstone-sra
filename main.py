2353a528d64450aca3d38dd09240fbc04f0ec1f7
nblarimore <nblarimore@ualr.edu>
2021-12-22 17:56:32 -0600

    Initial deployment

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import psycopg2
 
 app = create_app()
 
-DATABASE_URL = os.environ['DATABASE_URL']
-
 ENV ='prod'
 
 if ENV == 'dev':
@@ -14,7 +12,7 @@ if ENV == 'dev':
 else:
     app.debug = False
     app.config['SQLACHEMY_DATABASE_URI'] = 'postgres://mhgqtisxrnhajo:47830ca44a024943e000deda5eb86a37b84fb1e4dd79bec1cc233702ce32a99e@ec2-3-225-132-26.compute-1.amazonaws.com:5432/d3m22ojr7fksk2'
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    
     
 
 if __name__ == '__main__':
