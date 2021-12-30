044e47e336dcee89e20fe5ec8cd80c6232b324bc
smjp7711 <78757005+smjp7711@users.noreply.github.com>
2021-12-29 21:40:01 -0600

    added flask_mail
    Added button to send emails from sra_admin page.
    
    Using proton email inbox as sender
    as sendgrid

--- website/__init__.py
+++ website/__init__.py
@@ -3,6 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+#Adding mail variables
+from flask_mail import Mail
+mail = Mail()
+
 
 
 
@@ -11,7 +15,20 @@ db = SQLAlchemy()
 def create_app():
     
     app = Flask(__name__)
+
     app.config['SECRET_KEY'] = 'mr. worldwide'
+    #Configuration of mail
+    app.config['MAIL_SERVER']='smtp.sendgrid.net'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USERNAME'] = 'apikey'
+    app.config['MAIL_PASSWORD'] = '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'
+    app.config['MAIL_USE_SSL'] = False
+    
+    
+
+    mail.init_app(app)
+
     ENV ='prod'
     
     if ENV == 'dev':
@@ -26,7 +43,7 @@ def create_app():
     db.init_app(app)
 
 
-    from .views import views
+    from .views import views, sra_admin
     from .auth import auth
     
     app.register_blueprint(views, url_prefix='/')
@@ -43,6 +60,7 @@ def create_app():
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
+
     
     
     
@@ -52,3 +70,4 @@ def create_database(app):
     if not path.exists('website/' + 'sra'):
         db.create_all(app=app)
         print('Created Database!')
+
